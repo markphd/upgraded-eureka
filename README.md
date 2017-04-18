d2a98242c3f80e8c87035c9d3b211c4731f375e8
markphd <radical.mark@gmail.com>
2017-04-18 12:48:59 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# starter-kit
-Pug (jade), SASS, Browsersync, Gulp
+# ◈
+
+### Local-dev
+1. Install dependencies via `npm install` 
+2. Run `gulp`
+
+### Dependencies
+* [Gulp](https://github.com/gulpjs/gulp) - Gulp task runner
+* [gulp-pug](https://www.npmjs.com/package/gulp-pug) - Frontend JS template
+* [gulp-sass](https://www.npmjs.com/package/gulp-sass) - CSS preprocessor
+* [Browser-sync](https://www.npmjs.com/package/browser-sync) - Live CSS Reload & Browser Syncing
+
+### To-do
+    ✔ - Item 1
+    ✔ - Item 2
+    ❑ - Item 3
+    ❑ - Item 4
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var minifyCSS = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('templates', function(){
+
+	var data = {
+		pretty: true,
+		year: new Date().getFullYear()
+	};
+
+	return gulp.src('src/templates/*.jade')
+		.pipe(pug(data))
+		.pipe(gulp.dest('./'));
+})
+
+gulp.task('images', function(){
+	gulp.src(['src/img/**/*'])
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img'))
+		.pipe(browserSync.stream());
+})
+
+gulp.task('scripts', function(){
+	gulp.src(['src/scripts/**/*.js'])
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist/scripts'))
+		.pipe(browserSync.stream());
+})
+
+gulp.task('styles', function(){
+	gulp.src('src/styles/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(minifyCSS())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.stream());
+	})
+
+gulp.task('default', ['templates', 'styles', 'scripts'], function(){
+	browserSync.init({
+		server: './'
+	});
+	gulp.watch('src/templates/**/*.jade', ['templates']);
+	gulp.watch('src/img/**/*', ['images']);
+	gulp.watch('src/styles/**/*.scss', ['styles']);
+	gulp.watch('src/scripts/**/*.js', ['scripts']);
+	gulp.watch('*.html', browserSync.reload);
+})
+
