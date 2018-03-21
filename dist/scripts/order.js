8f0b002fb176bdaba8a463e5098c7f22d996c99e
Mark Philip Dawal <radical.mark@gmail.com>
2018-03-21 23:26:21 +0400

    implemented data for domain basket

--- dist/scripts/order.js
+++ dist/scripts/order.js
@@ -1,54 +1,65 @@
+console.clear()
+
+// BASKET - Initial Order List 
+
 const orderBasket = [
 	{
-		domain: "markphd.com",
-		package: "starter",
+		domain: "forup.dk",
+		package: "Starter",
 		addons: [],
 		price: 0
 	},
 	{
-		domain: "markphd.com",
-		package: "starter",
+		domain: "forup.one",
+		package: "Professional Plus",
 		addons: [],
 		price: 0
 	},
 	{
-		domain: "markphd.com",
-		package: "starter",
+		domain: "forup.org",
+		package: "Business",
 		addons: [],
 		price: 0
 	}
 ]
+
+// ORDER STEPS - Navigation sticky
+
 const scrollStream = Rx.Observable.fromEvent(window, 'scroll');
 
-
 scrollStream.debounceTime(10).subscribe( (value) => {
-	console.log(value.pageY, "value Y")
+	// console.log(value.pageY, "value Y")
 	let stickyNav = document.querySelector('#nav--order-steps');
 	if(value.pageY >= 150){
 		stickyNav.classList.add('fixed')
-		console.log(stickyNav)
+		// console.log(stickyNav)
 	} else {
 		stickyNav.classList.remove('fixed')
 	}
 })
 
-const orderStream = Rx.Observable.of(orderBasket)
-const addExtraStream = Rx.Observable.fromEvent(document.querySelectorAll('.order--extra-select'), 'click');
-// const addExtraStream = Rx.Observable.fromEvent(document.querySelectorAll('.order--extra-select'), 'click');
+// BASKET - Add/Remove Order in Basket
 
-const subject = new Rx.BehaviorSubject(orderBasket);
+const orderStream = Rx.Observable.of(orderBasket) // Stream of order activities
+
+// Add order & extra add-ons
+const addExtraStream = Rx.Observable.fromEvent(document.querySelectorAll('.order--extra-select'), 'click');
+const addDomainStream = Rx.Observable.fromEvent(document.querySelectorAll('.order--domain-select'), 'click');
+
+const subject = new Rx.Subject();
 
 addExtraStream.subscribe( (event) => {
-	for(i in orderBasket){
-		orderBasket[i].addons.push(event.target.value)
-	}
+	() => { for(i in orderBasket){
+		subject.next(orderBasket[i].addons.push(event.target.value))
+	}}
 	console.log(subject.getValue())
 })
 
 
-orderStream.map( (value)=> {
-	console.log(value)
-})
+
+// subject.subscribe( (value) => console.log(value) )
+// orderStream.map( (value)=> console.log(value) )
+
 
 const orderStep1 = Rx.Observable.fromEvent(document.querySelector('.order--step-product'), 'click');
 
@@ -101,3 +112,36 @@ orderStep4.subscribe( (event) => {
 		return null;
 	}
 })
+
+
+
+// BASKET - Info domain list
+let productList = document.querySelector('#order--domain-list');
+
+productList.innerHTML = orderBasket.map( (order) => `
+	<div class="buycustomer__addon__domain">
+	    <input id="${order.domain}" checked="checked" type="checkbox">
+	    <label for="${order.domain}"><span></span><span class="domain-label">${order.domain}</span>
+	        <span class="right">
+	            <span class="price-label">
+	                <div class="current-package product-hosting-bg active">
+	                    <span>${order.package}</span>
+	                    <span class="package--selector">▾</span>
+	                </div>
+	            </span>
+	        </span>
+	    </label>
+
+	    <span class="order--details-feat">Hosting Starter (25 GB) - 12 months   <em>240.00</em></span>
+	    <span class="order--details-feat">Domain fee - first 12 months (.cc)  <em>150.00</em></span>
+	    <span class="order--details-feat">Setup fee   <em>115.00</em></span>
+
+	    <input id="${order.domain}Privacy" name="${order.domain}Privacy" type="checkbox">
+
+	    <label for="${order.domain}Privacy">
+	    <span></span>
+	    <span class="domain-label">Domain Privacy - 12 months</span>
+	    </label>
+	</div>
+	`
+).join(' ')
