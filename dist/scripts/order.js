0f881f54338b86d7ec685676d91e8c8d51801753
Mark <radical.mark@gmail.com>
2018-03-26 17:47:42 +0400

    fix form input style

--- dist/scripts/order.js
+++ dist/scripts/order.js
@@ -45,9 +45,9 @@ let customerRegFormNew = `
                         <label id="labelCustomerCode" for="customerCode">Date of birth (dd-mm-yyyy):</label>
                     </div>
                     <div class="form--input-wrapper">
-                        <select id="birthdate_day" class="birthdate form--order-input" style="order: 1;"><option>Day..</option><option value="01">1</option><option value="02">2</option><option value="03">3</option><option value="04">4</option><option value="05">5</option><option value="06">6</option><option value="07">7</option><option value="08">8</option><option value="09">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option></select>
-                        <select id="birthdate_month" class="birthdate form--order-input" style="order: 2;"><option>Month..</option><option value="01">1</option><option value="02">2</option><option value="03">3</option><option value="04">4</option><option value="05">5</option><option value="06">6</option><option value="07">7</option><option value="08">8</option><option value="09">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option></select>
-                        <select id="birthdate_year" class="birthdate form--order-input" style="order: 3;"><option>Year..</option><option value="1908">1908</option><option value="1909">1909</option><option value="1910">1910</option><option value="1911">1911</option><option value="1912">1912</option><option value="1913">1913</option><option value="1914">1914</option><option value="1915">1915</option><option value="1916">1916</option><option value="1917">1917</option><option value="1918">1918</option><option value="1919">1919</option><option value="1920">1920</option><option value="1921">1921</option><option value="1922">1922</option><option value="1923">1923</option><option value="1924">1924</option><option value="1925">1925</option><option value="1926">1926</option><option value="1927">1927</option><option value="1928">1928</option><option value="1929">1929</option><option value="1930">1930</option><option value="1931">1931</option><option value="1932">1932</option><option value="1933">1933</option><option value="1934">1934</option><option value="1935">1935</option><option value="1936">1936</option><option value="1937">1937</option><option value="1938">1938</option><option value="1939">1939</option><option value="1940">1940</option><option value="1941">1941</option><option value="1942">1942</option><option value="1943">1943</option><option value="1944">1944</option><option value="1945">1945</option><option value="1946">1946</option><option value="1947">1947</option><option value="1948">1948</option><option value="1949">1949</option><option value="1950">1950</option><option value="1951">1951</option><option value="1952">1952</option><option value="1953">1953</option><option value="1954">1954</option><option value="1955">1955</option><option value="1956">1956</option><option value="1957">1957</option><option value="1958">1958</option><option value="1959">1959</option><option value="1960">1960</option><option value="1961">1961</option><option value="1962">1962</option><option value="1963">1963</option><option value="1964">1964</option><option value="1965">1965</option><option value="1966">1966</option><option value="1967">1967</option><option value="1968">1968</option><option value="1969">1969</option><option value="1970">1970</option><option value="1971">1971</option><option value="1972">1972</option><option value="1973">1973</option><option value="1974">1974</option><option value="1975">1975</option><option value="1976">1976</option><option value="1977">1977</option><option value="1978">1978</option><option value="1979">1979</option><option value="1980">1980</option><option value="1981">1981</option><option value="1982">1982</option><option value="1983">1983</option><option value="1984">1984</option><option value="1985">1985</option><option value="1986">1986</option><option value="1987">1987</option><option value="1988">1988</option><option value="1989">1989</option><option value="1990">1990</option><option value="1991">1991</option><option value="1992">1992</option><option value="1993">1993</option><option value="1994">1994</option><option value="1995">1995</option><option value="1996">1996</option><option value="1997">1997</option><option value="1998">1998</option><option value="1999">1999</option><option value="2000">2000</option></select>
+                        <select id="birthdate_day" class="birthdate form--order-input" style="order: 1;"><option>Day</option><option value="01">1</option><option value="02">2</option><option value="03">3</option><option value="04">4</option><option value="05">5</option><option value="06">6</option><option value="07">7</option><option value="08">8</option><option value="09">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option></select>
+                        <select id="birthdate_month" class="birthdate form--order-input" style="order: 2;"><option>Month</option><option value="01">1</option><option value="02">2</option><option value="03">3</option><option value="04">4</option><option value="05">5</option><option value="06">6</option><option value="07">7</option><option value="08">8</option><option value="09">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option></select>
+                        <select id="birthdate_year" class="birthdate form--order-input" style="order: 3;"><option>Year</option><option value="1908">1908</option><option value="1909">1909</option><option value="1910">1910</option><option value="1911">1911</option><option value="1912">1912</option><option value="1913">1913</option><option value="1914">1914</option><option value="1915">1915</option><option value="1916">1916</option><option value="1917">1917</option><option value="1918">1918</option><option value="1919">1919</option><option value="1920">1920</option><option value="1921">1921</option><option value="1922">1922</option><option value="1923">1923</option><option value="1924">1924</option><option value="1925">1925</option><option value="1926">1926</option><option value="1927">1927</option><option value="1928">1928</option><option value="1929">1929</option><option value="1930">1930</option><option value="1931">1931</option><option value="1932">1932</option><option value="1933">1933</option><option value="1934">1934</option><option value="1935">1935</option><option value="1936">1936</option><option value="1937">1937</option><option value="1938">1938</option><option value="1939">1939</option><option value="1940">1940</option><option value="1941">1941</option><option value="1942">1942</option><option value="1943">1943</option><option value="1944">1944</option><option value="1945">1945</option><option value="1946">1946</option><option value="1947">1947</option><option value="1948">1948</option><option value="1949">1949</option><option value="1950">1950</option><option value="1951">1951</option><option value="1952">1952</option><option value="1953">1953</option><option value="1954">1954</option><option value="1955">1955</option><option value="1956">1956</option><option value="1957">1957</option><option value="1958">1958</option><option value="1959">1959</option><option value="1960">1960</option><option value="1961">1961</option><option value="1962">1962</option><option value="1963">1963</option><option value="1964">1964</option><option value="1965">1965</option><option value="1966">1966</option><option value="1967">1967</option><option value="1968">1968</option><option value="1969">1969</option><option value="1970">1970</option><option value="1971">1971</option><option value="1972">1972</option><option value="1973">1973</option><option value="1974">1974</option><option value="1975">1975</option><option value="1976">1976</option><option value="1977">1977</option><option value="1978">1978</option><option value="1979">1979</option><option value="1980">1980</option><option value="1981">1981</option><option value="1982">1982</option><option value="1983">1983</option><option value="1984">1984</option><option value="1985">1985</option><option value="1986">1986</option><option value="1987">1987</option><option value="1988">1988</option><option value="1989">1989</option><option value="1990">1990</option><option value="1991">1991</option><option value="1992">1992</option><option value="1993">1993</option><option value="1994">1994</option><option value="1995">1995</option><option value="1996">1996</option><option value="1997">1997</option><option value="1998">1998</option><option value="1999">1999</option><option value="2000">2000</option></select>
                     </div>
                     <div class="form--label-wrapper">
                         <label for="customerCountry">Country:</label>
@@ -159,34 +159,23 @@ let customerNewInputs = `
 
 let customerExistInputs = `
     <div class="formFields">
-            <div style="float: left;"
-                <label for="customerEmail" class="labelUsername" style="">
-                    Email (username):
-                </label>
+            <div class="form--label-wrapper">
+                <label id="userLogin" for="customerName">Email (username):</label>
             </div>
-            <label for="customerEmail" class="labelEmail" style="display: none;">Email:</label>
-            <div style="width: 460px;">
-                <input autocomplete="on" autocapitalize="off" class="border" id="customerEmail" name="customerEmail" value="" type="email">
+            <div class="form--input-wrapper">
+                <input name="customerName" value="" id="userLogin" class="form--order-input" type="text">
             </div>
+            <div class="form--label-wrapper">
+                <label id="userPassword" for="customerEmail">Password</label>
+            </div>
+            <div class="form--input-wrapper">
+                <input name="customerName" value="" id="userPassword" class="form--order-input" type="text">
+            </div>            
         
-
-        <div class="adminUserPasswordContainer" style="">
-                <div style="float: left;">
-                    <label for="adminUserPassword">Password:</label>
-                </div>
-                
-                <div style="width: 460px;">
-                    <input name="adminUserPassword" id="adminUserPassword" class="border" autocomplete="off" type="password">
-                </div>
-            <p>
-                <small class="text-italic">Enter your email address and your control panel password. The email address is the contact email address you entered for your web spaces.</small>
-            </p>
-        </div>
-    </div>
-    <div id="" class="mg-top-15">
-        <div class="forgotpassword" style="position: absolute; bottom: 30px;">
-            <a href="/admin/forgotpassword.do?locale=en" target="_blank">Forgot your password?</a>
-        </div>
+            <div class="form--input-helper">
+              <p>Enter your email address and your control panel password. The email address is the contact email address you entered for your web spaces.</p>
+              <a href="/" target="_blank">Forgot your password?</a>
+            </div>
     </div>
 `
 
@@ -196,6 +185,11 @@ const basketSidebar = `
       <span><a href="">Edit</a></span>
       <div class="order--summary-list">
 
+      </div>
+      <div class="order--price-summary">
+        <p>Total excl VAT  <em>EUR 32.00</em></p>
+        <p>+25% VAT <em>EUR 8.00</em></p>
+        <p>Total incl VAT <em>EUR 40.00</em></p>
       </div>
     </div>
 `
@@ -875,18 +869,8 @@ function basketLoader() {
 
     packageSelectStream.subscribe(e => {
             e.target.nextElementSibling.classList.toggle('show--tooltip')
-
-            // if (e.target.nextElementSibling.style.display == '') {
-            //     console.log(e.target.nextElementSibling.style.display)
-            //     e.target.nextElementSibling.style.display = 'block';
-            // } else if (e.target.nextElementSibling.style.display == 'block'){
-            //     console.log(e.target.nextElementSibling.style.display)
-            //     e.target.nextElementSibling.style.display = 'none'
-            // } else {
-            //     e.target.nextElementSibling.style.display = 'block'
-            // }
         })
-        // .subscribe( pair => console.log(pair) )
+
 
     let tooltipStream = Rx.Observable.fromEvent(document.querySelectorAll('.order--details-feat > dfn'), 'mouseover');
 
@@ -924,7 +908,6 @@ function basketSidebarLoader() {
             <span class="side--basket-addon">${service}</span>
             ` ).join(' ') }
             <hr/>
-            
         </div>
     `).join(' ')
 }
